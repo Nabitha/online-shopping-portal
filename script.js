77e6491fdba0bf722b10d6ee89038de73718bec4
Nabitha <nabithas53@gmail.com>
2022-01-16 16:33:14 +0530

    Ok

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 /* Your code goes here */
-      
+      const navSlide = function(){
+          const nav = document.querySelector('.nav')
+          const navigation = document.querySelector('.navigation')
+          const navLinks = document.querySelectorAll('.navigation li')
+          //Toggle nav
+          nav.addEventListener('click',()=>{
+              navigation.classList.toggle('navigation-active')
+          })
+          navigation.addEventListener('click',()=>{
+            navigation.classList.toggle('navigation-active')
+
+          })
+          //Animate links
+          navLinks.forEach((link, index)=>{
+            link.style.animation = `navLinkFade 0.5s ease forwards`
+          })
+      }
+      navSlide()
